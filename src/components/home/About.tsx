8ac9e9b782f962721ddc361e629590459e3ab9e1
3a1mtest-prog <3a1mtest@gmail.com>
2026-04-23 16:32:07 +0300

    Update About.tsx

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -59,7 +59,7 @@ const About = () => {
         autoAlpha: 1,
         y: 0,
         filter: "blur(0px)",
-        duration: 1.6,
+        duration: 1.4,
         ease: "power3.out",
       })
         // النقاط تنبثق بعدها بنعومة
