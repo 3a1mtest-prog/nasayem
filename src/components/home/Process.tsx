6ad3190a920f97df17b2f2efd2b5acb0bc86e365
3a1mtest-prog <3a1mtest@gmail.com>
2026-04-26 15:17:05 +0300

    Update Process.tsx

--- src/components/home/Process.tsx
+++ src/components/home/Process.tsx
@@ -72,7 +72,7 @@ const Process = () => {
       className="py-24 md:py-32 relative border-t border-text/10 bg-dark-bg"
     >
       <Container>
-        <h2 className="text-3xl md:text-4xl font-display font-bold mb-16 md:mb-32 text-center tracking-widest">
+        <h2 className="text-3xl md:text-4xl font-display font-bold mb-16 md:mb-32 text-center tracking-widest text-end">
           {t('process.heading')}
         </h2>
 
